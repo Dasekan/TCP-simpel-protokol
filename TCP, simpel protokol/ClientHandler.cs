dcf36893c5f788f71365e69c6f00c9514a1bc89c
Dasekan Allan Karim <dak003@edu.zealand.dk>
2025-03-18 01:09:54 +0100

    Add project files.

---
+++ TCP, simpel protokol/ClientHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP__simpel_protokol
+{
+    public static class ClientHandler
+    {
+        public static void HandleClient(TcpClient socket)
+        {
+            // Get the network stream to read/write data
+            NetworkStream stream = socket.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            while (true) // Keep handling client requests indefinitely
+            {
+                string message = reader.ReadLine();
+
+                switch (message)
+                {
+                    case "Random":
+                        writer.WriteLine("Input numbers");
+                        writer.Flush();
+
+                        string[] twoNumbers = reader.ReadLine().Split();
+                        Random random = new Random();
+                        int num1 = int.Parse(twoNumbers[0]);
+                        int num2 = int.Parse(twoNumbers[1]);
+                        string result = random.Next(num1, num2 + 1).ToString();
+                        writer.WriteLine(result);
+                        writer.Flush();
+                        break;
+
+                    case "Add":
+                        writer.WriteLine("Input numbers");
+                        writer.Flush();
+
+                        string[] addNum = reader.ReadLine().Split();
+                        int addNum1 = int.Parse(addNum[0]);
+                        int addNum2 = int.Parse(addNum[1]);
+                        string addResult = (addNum1 + addNum2).ToString();
+                        writer.WriteLine(addResult);
+                        writer.Flush();
+                        break;
+
+                    case "Subtract":
+                        writer.WriteLine("Input numbers");
+                        writer.Flush();
+
+                        string[] subNumbers = reader.ReadLine().Split();
+                        int subNum1 = int.Parse(subNumbers[0]);
+                        int subNum2 = int.Parse(subNumbers[1]);
+                        string subResult = (subNum1 - subNum2).ToString();
+                        writer.WriteLine(subResult);
+                        writer.Flush();
+                        break;
+                }
+                socket.Close();
+            }
+            
+        }
+    }
+}
---
+++ TCP, simpel protokol/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TCP__simpel_protokol;
+
+public class Program
+{
+    private static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 7);
+        listener.Start();
+        Console.WriteLine("Server is listening on port 7...");
+        while (true)
+        {
+            TcpClient socket = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+            Task.Run(() => ClientHandler.HandleClient(socket));
+        }
+    }
+
+}
